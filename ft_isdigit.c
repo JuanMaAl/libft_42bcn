4034bd36f3bdfc24cf58e5df16cff9e102e51850
Juan Martinez Alonso <juanmar2@cbr10s1.42barcelona.com>
2024-09-27 11:42:11 +0200

    isdigit reworked

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,14 +6,23 @@
 /*   By: juanmar2                                   +#+  +:+       +#+        */
 /*      <juanmar2@student.42barcelona.com>        +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 14:25:48 by juanmar2          #+#    #+#             */
-/*   Updated: 2024/09/23 14:26:01 by juanmar2         ###   ########.fr       */
+/*   Updated: 2024/09/27 11:40:17 by juanmar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
-		return (2048);
+		return (1);
 	else
 		return (0);
 }
+
+/*#include <stdio.h>
+int	main(int argc, char **argv)
+{
+	--argc;
+	int is_dig = ft_isdigit(argv[1][0]);
+	printf("the character %c is digit? %d\n", argv[1][0], is_dig);
+	return (0);
+}*/
