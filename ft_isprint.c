e227ac8cbddfd6c1bd1224740df7d4b56fd19a25
Juan Martinez Alonso <juanmar2@cbr11s2.42barcelona.com>
2024-09-30 17:44:00 +0200

    ft_isprint reworked

--- ft_isprint.c
+++ ft_isprint.c
@@ -3,17 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juanmar2                                   +#+  +:+       +#+        */
-/*      <juanmar2@student.42barcelona.com>        +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/23 20:59:32 by juanmar2          #+#    #+#             */
-/*   Updated: 2024/09/23 21:12:39 by juanmar2         ###   ########.fr       */
+/*   By: juanmar2 <@student.42barcelona.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 17:10:17 by juanmar2          #+#    #+#             */
+/*   Updated: 2024/09/30 17:41:42 by juanmar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isprint(int c)
 {
 	if (c >= 32 && c <= 126)
-		return (16384);
+		return (1);
 	else
 		return (0);
 }
+
+/*#include <stdio.h>
+int	main(void)
+{
+	printf("The character ! is printable? %d\n", ft_isprint(32));
+	printf("The character ~ is printable? %d\n", ft_isprint(126));
+	printf("The character NUL is printable? %d\n", ft_isprint(0));
+	printf("The character DEL is printable? %d\n", ft_isprint(127));
+	return (0);
+}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: juanmar2 <@student.42barcelona.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 14:47:41 by juanmar2          #+#    #+#             */
-/*   Updated: 2024/09/30 17:06:05 by juanmar2         ###   ########.fr       */
+/*   Updated: 2024/09/30 17:42:35 by juanmar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,6 @@ int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
+int	ft_isprint(int c);
 
 #endif 
