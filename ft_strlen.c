f3d9157769450cbda444a3f4af895d0e1c5e740a
Juan Martinez Alonso <juanmar2@cbr11s2.42barcelona.com>
2024-09-30 18:29:26 +0200

    ft_strlen reworked

--- ft_strlen.c
+++ ft_strlen.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juanmar2                                   +#+  +:+       +#+        */
-/*      <juanmar2@student.42barcelona.com>        +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/23 21:22:26 by juanmar2          #+#    #+#             */
-/*   Updated: 2024/09/23 21:26:24 by juanmar2         ###   ########.fr       */
+/*   By: juanmar2 <@student.42barcelona.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 18:15:29 by juanmar2          #+#    #+#             */
+/*   Updated: 2024/09/30 18:24:16 by juanmar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
 size_t	ft_strlen(const char *s)
 {
@@ -23,3 +23,14 @@ size_t	ft_strlen(const char *s)
 	}
 	return (i);
 }
+
+/*#include <string.h>
+#include <stdio.h>
+int	main(void)
+{
+	printf("strlen(Hola Mundo): %lu\n", strlen("Hola Mundo"));
+	printf("ft_strlen(Hola Mundo): %lu\n", ft_strlen("Hola Mundo"));
+	printf("strlen(nulo): %lu\n", strlen("\0"));
+	printf("ft_strlen(nulo): %lu\n", ft_strlen("\0"));
+	return (0);
+}*/
