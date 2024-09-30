506249c6cdcf8c2a0dc5ce8e05537c473968fc13
Juan Martinez Alonso <juanmar2@cbr11s2.42barcelona.com>
2024-09-30 19:13:37 +0200

    ft_memset reworked

--- ft_memset.c
+++ ft_memset.c
@@ -3,24 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juanmar2                                   +#+  +:+       +#+        */
-/*      <juanmar2@student.42barcelona.com>        +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/25 10:48:25 by juanmar2          #+#    #+#             */
-/*   Updated: 2024/09/25 19:25:45 by juanmar2         ###   ########.fr       */
+/*   By: juanmar2 <@student.42barcelona.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 18:32:24 by juanmar2          #+#    #+#             */
+/*   Updated: 2024/09/30 19:01:12 by juanmar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
+
+#include "libft.h"
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	char	*p;
+	unsigned char	*p;
+	size_t			i;
 
-	p = (char *)s;
-	while (n > 0)
-	{
-		*p = c;
-		p++;
-		n--;
-	}
+	p = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+		p[i++] = (unsigned char)c;
 	return (s);
 }
+
+/*#include <stdio.h>
+int	main(void)
+{
+	char	str1[4] = "Hola";
+
+	printf("str1 = %s\n", str1);
+	ft_memset(str1, 'z', 4);
+	printf("str1 despues de ft_memset(str1, z, 4): %s\n", str1);
+	return (0);
+}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: juanmar2 <@student.42barcelona.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 14:47:41 by juanmar2          #+#    #+#             */
-/*   Updated: 2024/09/30 18:21:28 by juanmar2         ###   ########.fr       */
+/*   Updated: 2024/09/30 19:02:08 by juanmar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@
 
 /*############################# FUNCTIONS ####################################*/
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *s, int c, size_t n);
 
 #endif 
